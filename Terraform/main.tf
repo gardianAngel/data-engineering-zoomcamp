887df08815eebafffa9c45411bab1937deaa6056
Unyimeabasi D. Abai <105404372+gardianAngel@users.noreply.github.com>
2026-04-05 16:30:32 +0000

    terraform and GCP

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -16,11 +16,29 @@ provider "google" {
   region  = var.region
 }
 
+# Get a temporary token for the service account
+data "google_service_account_access_token" "default" {
+  provider               = google
+  target_service_account = var.service_account_email
+  scopes                 = ["https://www.googleapis.com/auth/cloud-platform"]
+  lifetime               = "3600s"
+}
+
+# Use the temporary token for the impersonated provider
+provider "google" {
+  alias        = "impersonated"
+  access_token = data.google_service_account_access_token.default.access_token
+  project      = var.project_id
+  region       = var.region
+}
+
 locals {
   bucket_name = "my-prefix-${var.project_id}"
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  name     = local.bucket_name
-  location = var.region
+  provider                      = google.impersonated
+  name                          = local.bucket_name
+  location                      = var.region
+  uniform_bucket_level_access   = true
 }
--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -9,3 +9,9 @@ variable "region" {
   description = "GCP Region"
   default     = "us-central1"
 }
+
+variable "service_account_email" {
+  type        = string
+  description = "Service account email for impersonation"
+  default     = "terraform-runner@project-5397f083-cbf4-4f3e-9b2.iam.gserviceaccount.com"
+}
